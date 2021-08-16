84bdd734b939234af4d6ae66e02e7e110c2c4ff3
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-08-16 15:53:55 -0400

    Add files via upload

---
+++ docs/faq/index.jsx
@@ -0,0 +1,31 @@
+<div>
+    <link rel="stylesheet" href="style.css" />
+    <h1>Emerald FAQ</h1>
+    <details>
+        <summary>What is Emerald?</summary>
+        It's a programming language like JavaScript, Python, C, or Rust.
+    </details>
+    <details>
+        <summary>Is Emerald free?</summary>
+        Yes, it's free! In fact, we hate paying for stuff and we think you do too!
+    </details>
+    <details>
+        <summary>Does Emerald support Windows?</summary>
+        Yes! You just run <code>pfw.rb</code> and then run <code>windows.rb</code>. You should get a REPL popped up.
+    </details>
+    <details>
+        <summary>What language is Emerald written in?</summary>
+        Originally, it was written in Python, and that phase of developement was called 'Classic Emerald'. Then, there was a rewrite in Crystal, for better performance. That became 'Crystal Emerald'.
+        To answer your question, it's written in Crystal.
+    </details>
+    <details>
+        <summary>What is the <code>Hello, World!</code> program?</summary>
+        <pre>
+say Hello, World!
+        </pre>
+    </details>
+    <details>
+        <summary>Can Emerald be ran online?</summary>
+        Yes! We have an online runner, hosted <a href="http://emeraldrunner.ionllc.repl.co" target="_blank">here</a>. It even has a share button!
+    </details>
+</div>
